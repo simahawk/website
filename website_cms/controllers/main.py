fc0726e2bea3ee1ee9b64b31eb89df4582a2cf7d
Simone Orsi <simahawk@gmail.com>
2016-02-23 09:04:51 +0100

    cms: fix wording `context` -> `main_object` to be more compliant w/ odoo terms

--- website_cms/controllers/main.py
+++ website_cms/controllers/main.py
@@ -5,27 +5,30 @@ from openerp.http import request
 
 
 class ContextAwareMixin(object):
-    """`context` aware mixin klass."""
+    """`Context` aware mixin klass.
+
+    The `context` in this case is what odoo calls `main_object`.
+    """
 
     template = ''
 
-    def get_template(self, context, **post):
+    def get_template(self, main_object, **post):
         """Retrieve rendering template."""
         template = self.template
-        if hasattr(context, 'view_id') and context.view_id:
-            template = context.view_id.key
+        if hasattr(main_object, 'view_id') and main_object.view_id:
+            template = main_object.view_id.key
         if not template:
             raise NotImplementedError("You must provide a template!")
         return template
 
-    def get_render_values(self, context, **post):
+    def get_render_values(self, main_object, **post):
         """Retrieve rendering values.
 
-        Essentially we need 2 items: ``context`` and ``parent``.
+        Essentially we need 2 items: ``main_object`` and ``parent``.
 
-        The context by default is the item being traversed.
+        The main_object by default is the item being traversed.
         In other words: if you traverse the path to a page
-        that page will be the context.
+        that page will be the main_object.
 
         The parent - if any - is always the parent of the item being traversed.
 
@@ -33,26 +36,26 @@ class ContextAwareMixin(object):
 
             /cms/page-1/page-2
 
-        in this case, `page-2` is the context and `page-1` the parent.
+        in this case, `page-2` is the main_object and `page-1` the parent.
         """
 
         parent = None
-        if hasattr(context, 'parent_id'):
+        if hasattr(main_object, 'parent_id'):
             # get the parent if any
-            parent = context.parent_id
+            parent = main_object.parent_id
 
         values = {
-            'context': context,
+            'main_object': main_object,
             'parent': parent,
         }
         return values
 
-    def render(self, context, **post):
+    def render(self, main_object, **post):
         """Retrieve parameters for rendering and render view template.
         """
         return request.website.render(
-            self.get_template(context, **post),
-            self.get_render_values(context, **post),
+            self.get_template(main_object, **post),
+            self.get_render_values(main_object, **post),
         )
 
 
@@ -61,9 +64,9 @@ class PageController(http.Controller, ContextAwareMixin):
     template = 'website_cms.page_default'
 
     @http.route([
-        '/cms/<model("cms.page"):context>',
-        '/cms/<path:path>/<model("cms.page"):context>',
+        '/cms/<model("cms.page"):main_object>',
+        '/cms/<path:path>/<model("cms.page"):main_object>',
     ], type='http', auth='public', website=True)
-    def page(self, context, path=None, **post):
+    def page(self, main_object, path=None, **post):
         """Handle a `page` route."""
-        return self.render(context, **post)
+        return self.render(main_object, **post)
